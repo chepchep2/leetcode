93cb1ea927f9aefacef1f8193d5db8c1ac2fc9ad
chepchep2 <kajer102@gmail.com>
2025-12-12 16:12:54 +0900

    feat: 125번 다시 풀이

--- java/problems_test/Problem125Test.java
+++ java/problems_test/Problem125Test.java
@@ -1,16 +1,24 @@
 package problems_test;
 
 import problems.Problem125;
+import problems.Problem125_re;
 
 public class Problem125Test {
     public static void main(String[] args) {
         Problem125 sol = new Problem125();
+        Problem125_re solRe = new Problem125_re();
 
         String s1 = "A man, a plan, a canal: Panama";
         boolean result = sol.isPalindrome(s1);
         System.out.println("Test 1:");
         System.out.println("Expected: true");
         System.out.println("Output: " + result);
+        System.out.println();
+
+        boolean result2 = solRe.isPalindrome(s1);
+        System.out.println("Test 1_Re:");
+        System.out.println("Expected: true");
+        System.out.println("Output: " + result2);
         System.out.println();
 
         String s2 = "race a car";
@@ -20,9 +28,21 @@ public class Problem125Test {
         System.out.println("Output: " + result);
         System.out.println();
 
+        result2 = solRe.isPalindrome(s2);
+        System.out.println("Test 2_Re:");
+        System.out.println("Expected: false");
+        System.out.println("Output: " + result2);
+        System.out.println();
+
         String s3 = " ";
         result = sol.isPalindrome(s3);
         System.out.println("Test 3:");
+        System.out.println("Expected: true");
+        System.out.println("Output: " + result);
+        System.out.println();
+
+        result2 = solRe.isPalindrome(s3);
+        System.out.println("Test 3_Re:");
         System.out.println("Expected: true");
         System.out.println("Output: " + result);
         System.out.println();
