9dcab653ae70215fc3b34f29e3eaadd744debe79
chepchep2 <kajer102@gmail.com>
2025-12-16 22:57:39 +0900

    feat: DFS 사용해서 풀이

--- java/problems/Problem226.java
+++ java/problems/Problem226.java
@@ -2,34 +2,18 @@ package problems;
 
 import common.TreeNode;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Problem226 {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
         }
 
-        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-
-            TreeNode temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-
-            if (node.left != null) {
-                queue.add(node.left);
-            }
-
-            if (node.right != null) {
-                queue.add(node.right);
-            }
-        }
+        invertTree(root.left);
+        invertTree(root.right);
 
         return root;
     }
