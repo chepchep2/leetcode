58d8b082d3d9eb460bd78ee19bed5c00f73f4e30
chepchep2 <kajer102@gmail.com>
2025-12-16 22:15:44 +0900

    fix: 오타수정

--- java/common/TreeNode.java
+++ java/common/TreeNode.java
@@ -4,7 +4,7 @@ public class TreeNode {
     int val;
     public TreeNode left;
     public TreeNode right;
-    TreeNode() {};
+    TreeNode() {}
     public TreeNode(int val) {
         this.val = val;
     }
