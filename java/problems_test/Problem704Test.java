9e5df27334a106d79db4fd44df09774ef213546f
chepchep2 <kajer102@gmail.com>
2026-01-06 13:43:27 +0900

    feat: 704번 풀이(이진 탐색)

---
+++ java/problems_test/Problem704Test.java
@@ -0,0 +1,57 @@
+package problems_test;
+
+import problems.Problem704;
+
+public class Problem704Test {
+    public static void main(String[] args) {
+        Problem704 sol = new Problem704();
+
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        int result = sol.search(nums, target);
+        System.out.println("Test 1:");
+        System.out.println("Expected: 4");
+        System.out.println("Output: " + result);
+        System.out.println();
+
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        target = 2;
+        result = sol.search(nums, target);
+        System.out.println("Test 2:");
+        System.out.println("Expected: -1");
+        System.out.println("Output: " + result);
+        System.out.println();
+
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        target = 12;
+        result = sol.search(nums, target);
+        System.out.println("Test 3:");
+        System.out.println("Expected: 5");
+        System.out.println("Output: " + result);
+        System.out.println();
+
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        target = -1;
+        result = sol.search(nums, target);
+        System.out.println("Test 4:");
+        System.out.println("Expected: 0");
+        System.out.println("Output: " + result);
+        System.out.println();
+
+        nums = new int[]{5};
+        target = 5;
+        result = sol.search(nums, target);
+        System.out.println("Test 5:");
+        System.out.println("Expected: 0");
+        System.out.println("Output: " + result);
+        System.out.println();
+
+        nums = new int[]{2, 5};
+        target = 5;
+        result = sol.search(nums, target);
+        System.out.println("Test 5:");
+        System.out.println("Expected: 1");
+        System.out.println("Output: " + result);
+        System.out.println();
+    }
+}
