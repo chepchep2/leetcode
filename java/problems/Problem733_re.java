e43c420519da41dd4081f29fd7dd691bccf772d8
chepchep2 <kajer102@gmail.com>
2026-02-01 09:51:35 +0900

    feat: 733 문제 다시 풀이
    - 코드에 주석 달기

--- java/problems/Problem733_re.java
+++ java/problems/Problem733_re.java
@@ -1,12 +1,13 @@
 package problems;
 
-public class Problem733 {
+public class Problem733_re {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int startColor = image[sr][sc];
-
+        // 시작 색깔
         if (startColor == color) {
             return image;
         }
+        // 시작색깔이랑 바꾸려는 색상이랑 같으면 바꿀 필요없음
 
         dfs(image, sr, sc, startColor, color);
         return image;
@@ -16,16 +17,22 @@ public class Problem733 {
         if (row < 0 || row >= image.length || col < 0 || col >= image[0].length) {
             return;
         }
+        // 범위 설정
 
         if (image[row][col] != startColor) {
             return;
         }
+        // 현재색이랑 시작색이랑 다르면 바꿀 필요 없다.
 
         image[row][col] = color;
-
-        dfs(image,row -1, col, startColor, color);
-        dfs(image,row + 1, col, startColor, color);
-        dfs(image,row, col - 1, startColor, color);
-        dfs(image,row, col + 1, startColor, color);
+        // 수정
+        dfs(image, row - 1, col, startColor, color);
+        // 위
+        dfs(image, row + 1, col, startColor, color);
+        // 아래
+        dfs(image, row, col - 1, startColor, color);
+        // 왼쪽
+        dfs(image, row, col + 1, startColor, color);
+        // 오른쪽
     }
 }
--- java/problems_test/Problem733Test.java
+++ java/problems_test/Problem733Test.java
@@ -1,4 +1,34 @@
 package problems_test;
 
+import problems.Problem733;
+import problems.Problem733_re;
+
+import java.util.Arrays;
+
 public class Problem733Test {
+    public static void main(String[] args) {
+        Problem733 sol = new Problem733();
+        Problem733_re sol2 = new Problem733_re();
+
+        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        int sr = 1;
+        int sc = 1;
+        int color = 2;
+        int[][] result = sol2.floodFill(image, sr, sc, color);
+        System.out.println("Test 1:");
+        System.out.println("Expected: [[2, 2, 2],[2, 2, 0],[2, 0, 1]]");
+        System.out.println("Output: " + Arrays.deepToString(result));
+        System.out.println();
+
+        image = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        sr = 0;
+        sc = 0;
+        color = 0;
+        result = sol2.floodFill(image, sr, sc, color);
+        System.out.println("Test 2:");
+        System.out.println("Expected: [[0, 0, 0],[0, 0, 0],[0, 0, 0]]");
+        System.out.println("Output: " + Arrays.deepToString(result));
+        System.out.println();
+
+    }
 }
